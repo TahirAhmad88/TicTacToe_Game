aeb15b629254482877a77c216b5af4878154a5c6
Tahir Ahmad <tahirahmadhassukhel@gmail.com>
2026-06-28 10:20:07 +0500

    Enhance Tic Tac Toe game logic and UI
    Updated the game logic to display winner and draw messages with emojis. Improved the reset functionality to show/hide the game board.

--- TicTacToe.js
+++ TicTacToe.js
@@ -1,92 +1,91 @@
-/*Its about Tic Tac Toe. (Ninth Program) (Project).
-
-# To run this JavaScript program from VS Code in Browser run 
-its Html program by the name of "IndexOfLecture9.html" file in its folder.
-*/
+/* Its about Tic Tac Toe. (Ninth Program) (Project). */
 
 let boxes = document.querySelectorAll(".box");
 let resetBtn = document.querySelector("#reset-btn");
 let newGameBtn = document.querySelector("#new-btn");
-let msgContainer = document.querySelector(".msg-container"); //class
-let msg = document.querySelector("#msg"); //id
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
 
-let turnO = true; //PlayerX, PlayerO
+let turnO = true;
 
 const winPatterns = [
-    [0, 1, 2],
-    [0, 3, 6],
-    [0, 4, 8],
-    [1, 4, 7],
-    [2, 5, 8],
-    [2, 4, 6],
-    [3, 4, 5],
-    [6, 7, 8]
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
 ];
 
 const resetGame = () => {
-    turnO = true;
-    enableBoxes();
-    msgContainer.classList.add("hide");
+  turnO = true;
+  enableBoxes();
+  msgContainer.classList.add("hide");
+  // Make sure game board is visible
+  document.querySelector(".container").style.display = "flex";
 };
 
-boxes.forEach((box) =>{
-    box.addEventListener("click", () => {
-        console.log("Box was clicked");
-        if(turnO){
-            box.innerText = "O";
-            turnO = false;
-        }else {
-            box.innerText = "X";
-            turnO = true;
-        }
-        box.disabled = true;
-
-        checkWinner();
-    });
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (turnO) {
+      box.innerText = "O";
+      turnO = false;
+    } else {
+      box.innerText = "X";
+      turnO = true;
+    }
+    box.disabled = true;
+    checkWinner();
+  });
 });
 
 const disableBoxes = () => {
-    for(let box of boxes) {
-        box.disabled = true;
-    }
+  for (let box of boxes) {
+    box.disabled = true;
+  }
 };
 
 const enableBoxes = () => {
-    for(let box of boxes){
-        box.disabled = false;
-        box.innerText = "";
-    }
+  for (let box of boxes) {
+    box.disabled = false;
+    box.innerText = "";
+  }
 };
 
 const showWinner = (winner) => {
-    msg.innerText = `Congratulations, Winner is ${winner}`;
-    msgContainer.classList.remove("hide");
-    disableBoxes();
+  msg.innerText = `🎉 Winner is ${winner}`;
+  msgContainer.classList.remove("hide");
+  // Hide the game board, show message overlay
+  document.querySelector(".container").style.display = "none";
+  disableBoxes();
 };
 
-const checkDraw = () => {  
-    return [...boxes].every(box => box.innerText !== "");  
-};  
+const checkDraw = () => {
+  return [...boxes].every((box) => box.innerText !== "");
+};
 
 const checkWinner = () => {
-    for(let pattern of winPatterns) {
-        let pos1Val = boxes[pattern[0]].innerText;
-        let pos2Val = boxes[pattern[1]].innerText;
-        let pos3Val = boxes[pattern[2]].innerText;
+  for (let pattern of winPatterns) {
+    let pos1Val = boxes[pattern[0]].innerText;
+    let pos2Val = boxes[pattern[1]].innerText;
+    let pos3Val = boxes[pattern[2]].innerText;
 
-        if(pos1Val != "" && pos2Val != "" && pos3Val != ""){
-            if(pos1Val === pos2Val && pos2Val === pos3Val) {
-                showWinner(pos1Val);   
-                return; // Exit after finding a winner  
- 
-            }
-        }
+    if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+      if (pos1Val === pos2Val && pos2Val === pos3Val) {
+        showWinner(pos1Val);
+        return;
+      }
     }
-    if (checkDraw()) {  
-        msg.innerText = "It's a Draw! Well played";  
-        msgContainer.classList.remove("hide");  
-        disableBoxes();  
-    }  
+  }
+  if (checkDraw()) {
+    msg.innerText = "🤝 It's a Draw! Well played";
+    msgContainer.classList.remove("hide");
+    // Hide the game board, show message overlay
+    document.querySelector(".container").style.display = "none";
+    disableBoxes();
+  }
 };
 
 newGameBtn.addEventListener("click", resetGame);
